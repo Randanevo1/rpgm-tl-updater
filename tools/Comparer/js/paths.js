97faf8c68a4b5e474bc3dab04a6823668f7ed7ab
Randanevo1 <89418410+Randanevo1@users.noreply.github.com>
2022-10-09 01:19:25 -0500

    Added string.raw so so you don't have to  \\

--- tools/Comparer/js/paths.js
+++ tools/Comparer/js/paths.js
@@ -1,3 +1,1 @@
-var $paths = {"old":, "new":};
-// paths need all \ to be replaced with \\ and spaces need / 
-// example path "old":"C:\\Users\\user\\Desktop\\game/ with/ spaces\\www\\data"
+var $paths = {"old":String.raw``, "new":String.raw``};
